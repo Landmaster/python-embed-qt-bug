e7ed24a972984fcf72b6be148fd328b7ba71110e
Landmaster <python0429@gmail.com>
2022-05-20 08:54:43 -0400

    add comment describing bug

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
+    // when line is uncommented, Py_BytesMain fails to boot the interactive shell and returns 0
+    // furthermore, qIsNull doesn't have the problem
     //std::cerr << qIsFinite(78.0) << std::endl;
     return Py_BytesMain(argc, argv);
 }
